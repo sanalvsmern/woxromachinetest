5845a33251039ec16e3662d33b7bf137645d1121
sanalvsmern <sanalvsmern@gmail.com>
2024-07-13 03:53:23 +0530

    Completed commit

--- src/Display.js
+++ src/Display.js
@@ -1,76 +1,70 @@
 import React, { useEffect, useState } from 'react'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Container from 'react-bootstrap/Container';
 
 function Display() {
-
     const [dirName, setDirName] = useState('');
     const [movName, setMovName] = useState('');
-    const [list, setList] = useState([{}])
+    const [list, setList] = useState([]);
 
-
-    const handleButtonAdd = () => {
+    const handleAdd = (e) => {
+        e.preventDefault()
         const newObject = { dirName: dirName, movName: movName };
-        setList([...list, newObject])
+        setList([...list, newObject]);
+        setDirName('');
+        setMovName('');
     }
 
-    console.log(dirName, movName);
+    const handleDelete = (i) => {
+        const newList = list.filter((item, j) => j !== i);
+        setList(newList);
+    }
+
+
     console.log(list);
 
-
     return (
-        <Container fluid>
-            <Row>
-                <Col md={12} className='heading'>
-                    <p>Director and Movies</p>
-                </Col>
-            </Row>
-            <Row>
-                <Col md={5} className='inputData'>
-                    <InputGroup className="mb-3">
-                        <Form.Control
-                            placeholder="Director Name"
-                            onChange={(e) => setDirName(e.target.value)}
-                        />
-                    </InputGroup>
-                </Col>
-                <Col md={5}>
-                    <InputGroup className="mb-3">
-                        <Form.Control
-                            placeholder="Movie Name"
-                            onChange={(e) => setMovName(e.target.value)}
-                        />
-                    </InputGroup>
-                </Col>
-                <Col md={2} className='buttonClass'>
-                    <Button variant="success" onClick={() => handleButtonAdd()}>Add</Button>
-                </Col>
-            </Row>
-
-{/* ...................................................................... */}
-            <Row>
-                {/* The following col s will be iterated multiple times by maping the list array,
-each li will have the key as corresponging index number. The onClicking the remove button, handleRemove function
-shall be triggered, and delete the correspondinig index item by filter method  */}
-                <Col md={5}>
-                    <div className='listing'>
-                        <p> {`${dirName}`}</p>
-                    </div>
-                </Col>
-                <Col md={5}>
-                    <div className='listing'>
-                        <p> {`${movName}`}</p>
-                    </div>
-                </Col>
-                <Col md={2} className='buttonClass'>
-                    <Button variant="danger">Remove</Button>
-                </Col>
-            </Row>
-        </Container>
+        <div className='allContents'>
+            <form onSubmit={handleAdd}>
+                <Row>
+                    <Col md={12} className='headerClass'>
+                        <p>Director and Movies</p>
+                    </Col>
+                </Row>
+                <Row className='mt-3 mb-3'>
+                    <Col md={5} className='d-flex justify-content-center'>
+                        <input className='inputBox'
+                            placeholder='Director Name'
+                            value={dirName}
+                            onChange={(e) => setDirName(e.target.value)}>
+                        </input>
+                    </Col>
+                    <Col md={5} className='d-flex justify-content-center'>
+                        <input className='inputBox'
+                            placeholder='Movie Name'
+                            value={movName}
+                            onChange={(e) => setMovName(e.target.value)}>
+                        </input>
+                    </Col>
+                    <Col md={2} className='button'>
+                        <button className='addButton' type='submit'> Add </button>
+                    </Col>
+                </Row>
+            </form>
+            {list.map((i, index) =>
+                <Row key={index}>
+                    <Col md={5} className='d-flex justify-content-center'>
+                        <p className='listItems'>{i.dirName}</p>
+                    </Col>
+                    <Col md={5} className='d-flex justify-content-center'>
+                        <p className='listItems'>{i.movName}</p>
+                    </Col>
+                    <Col md={2} className='button'>
+                        <button className='removeButton' onClick={() => handleDelete(index)}>Remove</button>
+                    </Col>
+                </Row>
+            )}
+        </div>
     )
 }
 
